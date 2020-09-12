ff43799d03677b4bff49c0494c9b9343a7a9e6fa
oh980225 <oh980225@gmail.com>
2020-09-12 18:30:12 +0900

    2020.09.12 COMMIT

--- WebCompiler/src/main/java/org/dms/web/ProblemController.java
+++ WebCompiler/src/main/java/org/dms/web/ProblemController.java
@@ -30,11 +30,47 @@ public class ProblemController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
+	// 여기가 id 받아서 해당문제
 	@RequestMapping(value="/problem/{problem_id}", method=RequestMethod.GET)
 	public String problemGet(@PathVariable("problem_id") int problem_id, Model model) throws Exception {
 		//logger.info("踰덊샇: "+ problem_id);
 		ProblemVO pvo = problemService.readProblem(problem_id);
 		TestcaseVO tvo= testcaseService.readTestcase(problem_id);
+		
+		// input, output 리스트를 받아온다.
+		List<String> tvoInput = testcaseService.getTestCaseInput(problem_id);
+		List<String> tvoOutput = testcaseService.getTestCaseOutput(problem_id);
+		
+		// console 창 출력
+		for(String input : tvoInput) {
+			System.out.println(input);
+		}
+		for(String output : tvoOutput) {
+			System.out.println(output);
+		}
+		
+		// file로 만들기
+		try {
+		    OutputStream file = new FileOutputStream("C:\\Users\\ohseu\\Desktop\\testcase.txt");
+		    
+		    String str = "input\n";
+		    for(String input : tvoInput) {
+				str += input + "\n";
+			}
+		    
+		    str += "output\n";
+		    for(String output : tvoOutput) {
+				str += output + "\n";
+			}
+		    
+		    byte[] by=str.getBytes();
+		    file.write(by);
+		    file.flush();
+		    file.close();
+				
+		} catch (Exception e) {
+	            e.getStackTrace();
+		}
 		
 		model.addAttribute("problem", pvo);
 		model.addAttribute("testcase", tvo);
--- WebCompiler/src/main/java/org/dms/web/persistence/TestcaseDAOImpl.java
+++ WebCompiler/src/main/java/org/dms/web/persistence/TestcaseDAOImpl.java
@@ -1,5 +1,7 @@
 package org.dms.web.persistence;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.dms.web.domain.TestcaseVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,18 @@ public class TestcaseDAOImpl implements TestcaseDAO {
 	public TestcaseVO read(int problem_id) throws Exception {
 		// TODO Auto-generated method stub
 		return sqlSession.selectOne(namespace + ".testcase_selectByid", problem_id);
+	}
+	
+	@Override
+	public List<String> getInputList(int problem_id) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectList(namespace + ".testcase_input_selectList", problem_id);
+	}
+	
+	@Override
+	public List<String> getOutputList(int problem_id) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectList(namespace + ".testcase_output_selectList", problem_id);
 	}
 
 }
